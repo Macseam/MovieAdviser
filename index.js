e8dfe3ee5ce3b837a7fcd5ef70dba7f2a0d0c405
Максим Швецов <MSShvetsov@dasreda.ru>
2019-02-12 12:36:44 +0300

    Доработал получение окружения

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.get('/cover', async function (req, res) {
     }
 });
 
-const NODE_ENV = process.env.NODE_ENV || app.get('env') || 'development';
+const NODE_ENV = app.get('env');
 const port = NODE_ENV !== 'development' ? 8080 : 3000;
 console.log(process.env.NODE_ENV)
 console.log(app.get('env'))
